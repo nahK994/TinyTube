517f371fe20cc659bcff8530000259f58df31045
nahK994 <nkskl6@gmail.com>
2024-10-25 02:59:57 +0600

    update run script

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo "--- AUTH ---"
 echo "1) Start APP"
 echo "2) Kill APP"
@@ -12,34 +14,51 @@ echo "8) Start DB"
 echo "9) Kill DB"
 echo "10) Run PLAYGROUND"
 echo "------------------"
-
+echo "--- RABBITMQ ---"
+echo "31) Start"
+echo "32) Kill"
+echo "------------------"
 read -p "Type: " cmd
 
-if [[ $cmd -le 5 ]]; then
-    cd backend/services/auth/
-elif [[ $cmd -gt 6 && $cmd -le 10 ]]; then
-    cd backend/services/user-management/
+
+if [[ $cmd -ge 1 && $cmd -le 5 ]]; then
+    cd backend/services/auth/ || exit
+elif [[ $cmd -ge 6 && $cmd -le 10 ]]; then
+    cd backend/services/user-management/ || exit
 else
     echo "Invalid option"
     exit 1
 fi
 
 
+if [[ $cmd == 31 ]]; then
+    cd backend/
+    docker compose -f rabbitmq.yml up -d || exit
+    cd ..
+    exit 0
+elif [[ $cmd == 32 ]]; then
+    cd backend/
+    docker compose -f rabbitmq.yml down || exit
+    cd ..
+    exit 0
+fi
+
+
 if [[ $((cmd % 5)) == 1 ]]; then
-    cd cmd/run/
-    go run main.go
+    cd cmd/run/ || exit
+    go run main.go || exit
     cd ../..
 elif [[ $((cmd % 5)) == 2 ]]; then
-    cd cmd/kill/
-    go run main.go
+    cd cmd/kill/ || exit
+    go run main.go || exit
     cd ../..
 elif [[ $((cmd % 5)) == 3 ]]; then
-    docker compose -f db.yml up -d
+    docker compose -f db.yml up -d || exit
 elif [[ $((cmd % 5)) == 4 ]]; then
-    docker compose -f db.yml down
+    docker compose -f db.yml down || exit
 elif [[ $((cmd % 5)) == 0 ]]; then
-    cd cmd/playground/
-    go run main.go
+    cd cmd/playground/ || exit
+    go run main.go || exit
     cd ../..
 fi
 
