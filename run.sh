96729a802a4c65514e876c886ca6e9acfe17e008
nahK994 <nkskl6@gmail.com>
2024-11-11 02:30:19 +0600

    update run.sh

--- run.sh
+++ run.sh
@@ -18,29 +18,43 @@ echo "--- RABBITMQ ---"
 echo "31) Start"
 echo "32) Kill"
 echo "------------------"
+echo "---- PGADMIN ----"
+echo "33) Start"
+echo "34) Kill"
+echo "------------------"
 read -p "Type: " cmd
 
 if [[ $cmd == 31 ]]; then
-    pushd backend/ || exit
+    pushd backend/config/ || exit
     docker compose -f rabbitmq.yml up -d && echo "RabbitMQ started"
     popd || exit
     exit 0
 elif [[ $cmd == 32 ]]; then
-    pushd backend/ || exit
+    pushd backend/config/ || exit
     docker compose -f rabbitmq.yml down && echo "RabbitMQ stopped"
+    popd || exit
+    exit 0
+elif [[ $cmd == 33 ]]; then
+    pushd backend/config/ || exit
+    docker compose -f pgadmin.yml up -d && echo "PGADMIN started"
+    popd || exit
+    exit 0
+elif [[ $cmd == 34 ]]; then
+    pushd backend/config/ || exit
+    docker compose -f pgadmin.yml down && echo "PGADMIN stopped"
     popd || exit
     exit 0
 fi
 
 app_dir=''
-db_dir=''
+db_file=''
 
 if [[ $cmd -ge 1 && $cmd -le 5 ]]; then
     app_dir=backend/services/auth/
-    db_dir=backend/db/auth/
+    db_file=auth.yml
 elif [[ $cmd -ge 6 && $cmd -le 10 ]]; then
     app_dir=backend/services/user-management/
-    db_dir=backend/db/user-management/
+    db_file=user-management.yml
 else
     echo "Invalid option"
     exit 1
@@ -55,18 +69,12 @@ elif [[ $((cmd % 5)) == 2 ]]; then
     go run main.go && echo "Auth/User-Management APP stopped"
     popd || exit
 elif [[ $((cmd % 5)) == 3 ]]; then
-    pushd "$db_dir" || exit
-    docker compose -f db.yml up -d && echo "Database started"
-    popd || exit
-    pushd "$db_dir/.." || exit
-    docker compose -f pgadmin.yml up -d && echo "pgAdmin started"
+    pushd backend/config/db/ || exit
+    docker compose -f $db_file up -d && echo "Database started"
     popd || exit
 elif [[ $((cmd % 5)) == 4 ]]; then
-    pushd "$db_dir" || exit
-    docker compose -f db.yml down && echo "Database stopped"
-    popd || exit
-    pushd "$db_dir/.." || exit
-    docker compose -f pgadmin.yml down && echo "pgAdmin stopped"
+    pushd backend/config/db/ || exit
+    docker compose -f $db_file down && echo "Database stopped"
     popd || exit
 elif [[ $((cmd % 5)) == 0 ]]; then
     pushd "$app_dir/cmd/playground/" || exit
