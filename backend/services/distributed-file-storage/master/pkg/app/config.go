80a95c1f361b188bb8b20001470a218713be3c23
nahK994 <nkskl6@gmail.com>
2024-11-21 23:08:45 +0600

    update dfs master config

--- backend/services/distributed-file-storage/master/pkg/app/config.go
+++ backend/services/distributed-file-storage/master/pkg/app/config.go
@@ -1,13 +1,21 @@
 package app
 
 type AppConfig struct {
-	Host string
-	Port int
+	Host    string
+	Port    int
+	Workers []string
 }
 
 func GetConfig() AppConfig {
 	return AppConfig{
 		Host: "127.0.0.1",
 		Port: 8002,
+		Workers: []string{
+			"http://localhost:8081",
+			"http://localhost:8082",
+			"http://localhost:8083",
+			"http://localhost:8084",
+			"http://localhost:8085",
+		},
 	}
 }
--- backend/services/distributed-file-storage/master/pkg/handlers/handlers.go
+++ backend/services/distributed-file-storage/master/pkg/handlers/handlers.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"dfs-master/pkg/app"
 	"dfs-master/pkg/db"
 	"fmt"
 	"io"
@@ -11,15 +12,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-var workerNodes = []string{
-	"http://localhost:8081",
-	"http://localhost:8082",
-	"http://localhost:8083",
-	"http://localhost:8084",
-	"http://localhost:8085",
-}
-
-// UploadHandler handles video uploads and distributes to a worker
 func UploadHandler(c *gin.Context) {
 	header, err := c.FormFile("file")
 	if err != nil {
@@ -27,13 +19,11 @@ func UploadHandler(c *gin.Context) {
 		return
 	}
 
-	// Save file metadata
 	fileID := db.SaveFileMetadata(header.Filename)
 
-	// Choose a worker node
-	worker := workerNodes[fileID%len(workerNodes)]
+	config := app.GetConfig()
+	worker := config.Workers[fileID%len(config.Workers)]
 
-	// Forward the file to the worker
 	fileContent, _ := header.Open()
 	defer fileContent.Close()
 	resp, err := http.Post(fmt.Sprintf("%s/store", worker), c.ContentType(), fileContent)
@@ -45,7 +35,6 @@ func UploadHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"message": fmt.Sprintf("File uploaded to %s", worker), "fileID": fileID})
 }
 
-// ReplicateHandler replicates a file to another worker node
 func ReplicateHandler(c *gin.Context) {
 	var req struct {
 		FileID int `json:"file_id"`
@@ -55,11 +44,10 @@ func ReplicateHandler(c *gin.Context) {
 		return
 	}
 
-	// Select the next worker for replication
-	worker := workerNodes[(req.FileID+1)%len(workerNodes)]
+	config := app.GetConfig()
+	worker := config.Workers[req.FileID%len(config.Workers)]
 	fileMetadata := db.GetFileMetadata(req.FileID)
 
-	// Forward replication request to the worker
 	data := url.Values{}
 	data.Set("filename", fileMetadata)
 
@@ -72,10 +60,10 @@ func ReplicateHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"message": fmt.Sprintf("File replicated to %s", worker)})
 }
 
-// RetrieveHandler retrieves a file from the appropriate worker
 func RetrieveHandler(c *gin.Context) {
 	fileID, _ := strconv.Atoi(c.Param("fileID"))
-	worker := workerNodes[fileID%len(workerNodes)]
+	config := app.GetConfig()
+	worker := config.Workers[fileID%len(config.Workers)]
 	resp, err := http.Get(fmt.Sprintf("%s/retrieve/%d", worker, fileID))
 	if err != nil || resp.StatusCode != http.StatusOK {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve file"})
